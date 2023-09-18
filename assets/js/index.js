f7bcddae4e4975ffbfd8a091d56df2f35d4682ea
camille <camille@cstar.io>
2023-09-18 15:55:00 +0200

    FIX card creation AND card remove

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,35 @@
 const currentDay = document.getElementById('currentDay');
 const cards = document.getElementsByClassName('card');
 const deleteButtons = document.getElementsByClassName('delete-button');
+const addCardButton = document.getElementById('addCardButton');
+document.addEventListener('DOMContentLoaded', function () {
+  const currentDay = document.getElementById('currentDay');
+  const cardManager = document.getElementById('cardManager');
 
+  // Le reste de votre code JavaScript va ici
+});
 let cardArray = [];
+let te = [];
 
 window.onload = () => {
   const newDate = new Date();
   const day = newDate.getDate() ;
-  const month = newDate.getMonth();
+  const month = newDate.getMonth() + 1;
   const year = newDate.getFullYear();
   currentDay.innerHTML = day.toString() + '/' + month.toString() + '/' + year.toString();
 }
 
+addCardButton.addEventListener('click', createCard);
+
 function createCard() {
   const card = new Card('title');
+  cardArray.push(card);
   card.createCard();
-  for (let i = 0; i < cards.length; i++) {
-    cards[i].setAttribute('id', card.id);
-    cardArray.push(cards[i]);
-  }
 }
 
-function deleteCard(cardId) {
-  let elementToDelete = null;
-  for (let i = 0; i < cards.length; i++) {
-    if (parseInt(cards[i].id) === cardId) {
-      elementToDelete = cards[i];
-    }
-  }
-  elementToDelete.remove();
-}
+// function deleteCard(cardId) {
+//   const cardElement = document.querySelector(`[data-card-id="${this.id}"]`);
+//   if (cardElement) {
+//     cardElement.remove();
+//   }
+// }
