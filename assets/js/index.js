ffaba2a6a8b753182eca3245ead2245efa56da58
camille <camille@cstar.io>
2023-09-19 10:15:51 +0200

    Add first try for local storage

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,22 +2,21 @@ const currentDay = document.getElementById('currentDay');
 const cards = document.getElementsByClassName('card');
 const deleteButtons = document.getElementsByClassName('delete-button');
 const addCardButton = document.getElementById('addCardButton');
+
 document.addEventListener('DOMContentLoaded', function () {
   const currentDay = document.getElementById('currentDay');
-  const cardManager = document.getElementById('cardManager');
 
-  // Le reste de votre code JavaScript va ici
-});
-let cardArray = [];
-let te = [];
-
-window.onload = () => {
+  // Affichage de la date du jour
   const newDate = new Date();
   const day = newDate.getDate() ;
   const month = newDate.getMonth() + 1;
   const year = newDate.getFullYear();
   currentDay.innerHTML = day.toString() + '/' + month.toString() + '/' + year.toString();
-}
+});
+
+
+
+let cardArray = [];
 
 addCardButton.addEventListener('click', createCard);
 
@@ -25,11 +24,10 @@ function createCard() {
   const card = new Card('title');
   cardArray.push(card);
   card.createCard();
+  localStorage.setItem('Card-' + card.id, card.id);
+  const maValeur = localStorage.getItem('Card');
+  console.log(localStorage);
+
 }
 
-// function deleteCard(cardId) {
-//   const cardElement = document.querySelector(`[data-card-id="${this.id}"]`);
-//   if (cardElement) {
-//     cardElement.remove();
-//   }
-// }
+
