606120673ecb01cca0c526a963f8e4a23182c57e
camille <camille@cstar.io>
2023-09-19 10:37:17 +0200

    FIX the mess I did

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,18 +6,7 @@ const addCardButton = document.getElementById('addCardButton');
 document.addEventListener('DOMContentLoaded', function () {
   const currentDay = document.getElementById('currentDay');
 
-<<<<<<< HEAD
-  // Le reste de votre code JavaScript va ici
-});
-const addCardButton = document.getElementById('addCardButton');
-document.addEventListener('DOMContentLoaded', function () {
-  const currentDay = document.getElementById('currentDay');
-  const cardManager = document.getElementById('cardManager');
-
-let cardArray = [];
-
-window.onload = () => {
-
+  // Affichage de la date du jour
   const newDate = new Date();
   const day = newDate.getDate() ;
   const month = newDate.getMonth() + 1;
@@ -31,36 +20,12 @@ let cardArray = [];
 
 addCardButton.addEventListener('click', createCard);
 
-addCardButton.addEventListener('click', createCard);
-
-
 function createCard() {
   const card = new Card('title');
   cardArray.push(card);
   card.createCard();
-<<<<<<< HEAD
-  for (let i = 0; i < cards.length; i++) {
-    cards[i].setAttribute('id', 'card-' + (i + 1));
-    cardArray.push(cards[i]);
-    console.log(cardArray);
-  }
-}
-
-function deleteCard(cardId) {
-  let elementToDelete = null;
-  for (let i = 0; i < cards.length; i++) {
-    if (parseInt(cards[i].id) === cardId) {
-      elementToDelete = cards[i];
-    }
-  }
-  elementToDelete.remove();
-}
-=======
   localStorage.setItem('Card-' + card.id, card.id);
   const maValeur = localStorage.getItem('Card');
   console.log(localStorage);
 
 }
-
-
->>>>>>> ffaba2a (Add first try for local storage)
