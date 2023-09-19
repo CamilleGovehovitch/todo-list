b624f408296e18710328fd386156a0292a51cb4b
camille <camille@cstar.io>
2023-09-19 10:15:51 +0200

    Add first try for local storage

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,4 +25,26 @@ function createCard() {
   const card = new Card('title');
   cardArray.push(card);
   card.createCard();
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].setAttribute('id', 'card-' + (i + 1));
+    cardArray.push(cards[i]);
+    console.log(cardArray);
+  }
 }
+
+function deleteCard(cardId) {
+  let elementToDelete = null;
+  for (let i = 0; i < cards.length; i++) {
+    if (parseInt(cards[i].id) === cardId) {
+      elementToDelete = cards[i];
+    }
+  }
+  elementToDelete.remove();
+}
+  localStorage.setItem('Card-' + card.id, card.id);
+  const maValeur = localStorage.getItem('Card');
+  console.log(localStorage);
+
+}
+
+
